63dc4f55c2d63b85edcb5f0cdfb7e589f9dcaf82
Keith <kclewis0614@gmail.com>
2020-11-01 14:30:40 -0600

    add settings.py back with ENVVAR protection

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.get('DJANGO_GIRLS_SECRET_TOKEN')
+SECRET_KEY = env.get('DJANGO_GIRLS_SECRET_TOKEN', "Environment variable does not exist")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
